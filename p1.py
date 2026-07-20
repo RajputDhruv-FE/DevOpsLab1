6f6beebaa93f31ed16749d3b42eaa7d4b4d8b5b9
Dhyey <dhyey1656@gmial.com>
2026-07-20 11:37:20 +0530

    do with the changes

--- p1.py
+++ p1.py
@@ -3,4 +3,4 @@ print("we are learning github")
 
 print("this is from feature 1")
 
-print("this is after loging in to the github")
+print("this is after loging in to the github, and changing the account")
