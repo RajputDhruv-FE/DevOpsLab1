c589d09c7041101f734bd43abe6a10967f4efec5
Dhruv <rajputdhruv443@gmail.com>
2026-07-20 11:44:55 +0530

    doneee

--- p1.py
+++ p1.py
@@ -3,4 +3,4 @@ print("we are learning github")
 
 print("this is from feature 1")
 
-print("this is after loging in to the github, and changing the account")
+print("this is after loging in to the github")
